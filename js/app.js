cfa586ab7b107eb65dd6ddfe55377327ad9807aa
Vahid97 <vahidefendi5@gmail.com>
2020-12-14 15:43:34 +0400

    js ilk elaveler

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+
+let liOneClick = queryselector(".li-1");
+let liTwoClick = queryselector(".li-2");
+let liThreeClick = queryselector(".li-3");
+let liFourClick = queryselector(".li-4");
+let liFiveClick = queryselector("li-5");
+
+
+let imgOne = queryselector("img-1");
+let imgTwo = queryselector("img-2");
+let imgThree = queryselector("img-3");
+let imgFour = queryselector("img-4");
+let imgFive = queryselector("img-5");
+
+liOneClick.addEventListener('click', function () {
+    imgOne.style.display = "block";
+})
